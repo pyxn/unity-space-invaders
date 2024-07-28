b64b67c5aa3a3771542794477b208bbbdecf39d4
pyxn <51064667+pyxn@users.noreply.github.com>
2024-07-27 20:06:29 -0400

    add basic movement using new input system

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rigidBody;
+    [SerializeField] private float moveSpeed = 5f;
+    private Vector2 _moveDirection;
+
+    [SerializeField] private InputActionReference move;
+    [SerializeField] private InputActionReference fire;
+
+
+    private void Update()
+    {
+        _moveDirection = move.action.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody.linearVelocity = new Vector2(_moveDirection.x * moveSpeed, _moveDirection.y * moveSpeed);
+    }
+
+
+    /*
+    FIRE METHOD
+     */
+    private void OnEnable()
+    {
+        fire.action.started += Fire;
+    }
+
+    private void OnDisable()
+    {
+        fire.action.started -= Fire;
+    }
+
+    private void Fire(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Fired");
+    }
+}
